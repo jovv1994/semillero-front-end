828422f0659ede0085f8a6cbcde2d630829bc2a8
Erick Cordova G <ericksitocg@hotmail.com>
2021-10-15 22:32:10 -0500

    Update: carpeta de customEvents con form ECG

---
+++ CustomEvents-impl/g2bmm.grupoc.customevents.impl/src/main/java/com/cobiscorp/cobis/g2bmm/customevents/impl/view/executecommand/VA_VABUTTONQUQNQDY_902390.java
@@ -0,0 +1,64 @@
+/*
+ * Archivo: VA_VABUTTONQUQNQDY_902390.java
+ *
+ * Esta aplicacion es parte de los paquetes bancarios propiedad de COBISCORP.
+ * Su uso no autorizado queda expresamente prohibido asi como cualquier
+ * alteracion o agregado hecho por alguno de sus usuarios sin el debido
+ * consentimiento por escrito de COBISCORP.
+ * Este programa esta protegido por la ley de derechos de autor y por las
+ * convenciones internacionales de propiedad intelectual. Su uso no
+ * autorizado dara derecho a COBISCORP para obtener ordenes de secuestro
+ * o retencion y para perseguir penalmente a los autores de cualquier infraccion.
+ */
+
+package com.cobiscorp.cobis.g2bmm.customevents.impl.view.executecommand;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+import com.cobiscorp.cobis.commons.domains.log.ILogger;
+import com.cobiscorp.cobis.commons.log.LogFactory;
+import com.cobiscorp.cobis.g2bmm.model.Estudiante;
+import com.cobiscorp.designer.api.DataEntity;
+import com.cobiscorp.designer.api.DynamicRequest;
+import com.cobiscorp.designer.api.customization.IExecuteCommand;
+import com.cobiscorp.designer.api.customization.arguments.IExecuteCommandEventArgs;
+import com.cobiscorp.designer.api.managers.DesignerManagerException;
+
+@Component
+@Service({ IExecuteCommand.class })
+@Properties(value={
+		@Property(name = "view.id", value = "VW_ESTUDIANCT_390"),
+		@Property(name = "view.version", value = "1.0.0"),
+		@Property(name = "view.controlId", value = "VA_VABUTTONQUQNQDY_902390")})
+
+public class VA_VABUTTONQUQNQDY_902390 implements IExecuteCommand {
+	/**
+	 * Instancia de Logger
+	 */
+	private static final ILogger logger = LogFactory.getLogger(VA_VABUTTONQUQNQDY_902390.class);
+
+	@Override
+	public void executeCommand(DynamicRequest entities, IExecuteCommandEventArgs arg1) {
+		// TODO Auto-generated method stub
+		try {
+			if (logger.isDebugEnabled()) {
+				logger.logDebug("Start executeCommand in VA_VABUTTONQUQNQDY_902390");
+			}
+			
+			DataEntity entityEstudiante = entities.getEntity(Estudiante.ENTITY_NAME);
+			String nombre = entityEstudiante.get(Estudiante.NOMBRE);
+			String apellido = entityEstudiante.get(Estudiante.APELLIDO);
+			
+			logger.logDebug("Nombre: " + nombre);
+			logger.logDebug("Apellido: " + apellido);
+			
+		} catch (Exception ex) {
+			DesignerManagerException.handleException(arg1.getMessageManager(), ex, logger);
+		}
+	}
+
+}
+
---
+++ G2BMM/FG2HX/forms/T_G2BMMMMFIHRQP_426/events/T_G2BMMMMFIHRQP_426_localVars.js
@@ -0,0 +1,1 @@
+/* variables locales de T_G2BMMMMFIHRQP_426*/
